7af923378f0c167f85ee6930ef520863ce815d35
ebrostay <info@ebrostay.com>
2026-08-03 22:34:35 +0200

    Prepare Jairo site for Google Calendar bookings

---
+++ jairo-portes/config.js
@@ -0,0 +1,8 @@
+window.EBROMOVE_CONFIG = {
+  bookingApiUrl: "",
+  bookingApiUseJsonp: true,
+  bookingToken: "",
+  calendarUrl: "",
+  whatsappPhone: "",
+  publicSiteUrl: "https://ebrostay.github.io/ebrostay-website-minimal/jairo-portes/"
+};
